95900c2bd5b79c72134d207e7395406dac72d677
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-03 14:44:28 -0500

    add main styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,16 @@
-// no need to reference path, use @ then folder + file name
 // import css stylesheet to apply the styles to every route in the application.
 import "../styles/globals.css";
+
+import Nav from "../components/Nav";
 
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
         <body>{children}</body>
+        <main className="main">
+        <Nav/>
+        </main>
+        
       </html>
     )
   }
---
+++ components/nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return (
+        <nav className="nav">
+            NavBar
+        </nav>
+    )
+}
+
+export default Nav;
