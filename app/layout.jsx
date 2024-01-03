42d4ec91e031177b6a7536ec69311e2b92241614
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-03 12:42:56 -0500

    add font style to globals, import globals to layout.jsx, delete jsconfig file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,7 @@
+// no need to reference path, use @ then folder + file name
+// import css stylesheet to apply the styles to every route in the application.
+import "../styles/globals.css";
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1>Travel Itinerary</h1>
+    return <h1>Scotland</h1>
   }
