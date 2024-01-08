072be6c0c93ad2805dcc1f418820bb20939f5712
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-08 14:13:04 -0500

    set up state and submit handler for DayForm

--- components/DayForm.jsx
+++ components/DayForm.jsx
@@ -1,16 +1,34 @@
+"use client"
+
+import React,  { useState } from 'react';
+
 const DayForm = () => {
+    const [ day, setDay ] = useState("")
+
+    const handleDay = (event) => {
+        setDay(event.target.value)
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(day)
+    };
+
     return(
-        <form className="day-form">
-            <label className="day-form" for='days'>How many days is your itinerary?</label>
-            <input className='day-form-input'
-                type='number' 
-                id='days' 
-                name='days'
-                min='1'
-                max='40'
-                placeholder='enter a number'
+        <form className="day-form" onSubmit={handleSubmit}>
+            <label htmlFor="days">How many days is your itinerary?</label>
+            <input className="day-form-input"
+                type="number" 
+                id="days" 
+                name="days"
+                min="1"
+                max="40"
+                placeholder="0"
+                value={day}
+                onChange={handleDay}
                 ></input>
-            <button className='day-form-btn'>Go!</button>
+            <button className="day-form-btn" type="submit">Go!</button>
         </form>
     )
 };
