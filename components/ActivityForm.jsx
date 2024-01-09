d2d594f755d0fc67c7e4ef6eed9bdcbcf3d6ad2f
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-09 15:35:12 -0500

    add base styling to ActivityForm

--- components/ActivityForm.jsx
+++ components/ActivityForm.jsx
@@ -2,16 +2,33 @@ const ActivityForm = () => {
     return (
         <section className="activity-form">
             <h3>Add an activity</h3>
-            <form >
-                <label>Title</label>
-                <input></input>
-                <label>Price</label>
-                <input></input>
-                <label>Address</label>
-                <input></input>
-                <label>info</label>
-                <input></input>
-                <button></button>
+            <form className="activity-form">
+                <label htmlFor="activity-title">Title</label>
+                <input className="activity-form-input"
+                type="text"
+                id="activity-title"
+                name="activity-title"
+                ></input>
+                <label htmlFor="activity-price">Price</label>
+                <input className="activity-form-input"
+                type="number"
+                id="activity-price"
+                name="activity-price"
+                placeholder="0"
+                ></input>
+                <label htmlFor="activity-address">Address</label>
+                <input className="activity-form-input"
+                type="text"
+                id="activity-address"
+                name="activity-address"
+                ></input>
+                <label htmlFor="activity-info">info</label>
+                <input className="activity-form-input"
+                type="text"
+                id="activity-info"
+                name="activity-info"
+                ></input>
+                <button className="btn-input-base-styling">Create</button>
             </form>
         </section>
                
