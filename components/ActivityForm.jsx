84505dedc6bf2402179f2e5b694431293c852bbf
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-09 17:31:15 -0500

    add className for ActivityForm labels

--- components/ActivityForm.jsx
+++ components/ActivityForm.jsx
@@ -3,25 +3,25 @@ const ActivityForm = () => {
         <section className="activity-form">
             <h3>Add an activity</h3>
             <form className="activity-form">
-                <label htmlFor="activity-title">Title</label>
+                <label htmlFor="activity-title" className="activity-label">Title</label>
                 <input className="small-text-input"
                 type="text"
                 id="activity-title"
                 name="activity-title"
                 ></input>
-                <label htmlFor="activity-address">Address</label>
-                <input className="small-text-input"
+                <label htmlFor="activity-address" className="activity-label">Address</label>
+                <input className="activity-input-short-text"
                 type="text"
                 id="activity-address"
                 name="activity-address"
                 ></input>
-                <label htmlFor="activity-info">Info</label>
+                <label htmlFor="activity-info" className="activity-label">Info</label>
                 <input className="large-text-input"
                 type="text"
                 id="activity-info"
                 name="activity-info"
                 ></input>
-                <label htmlFor="activity-price">Price</label>
+                <label htmlFor="activity-price" className="activity-label">Price</label>
                 <input className="price-input"
                 type="number"
                 id="activity-price"
