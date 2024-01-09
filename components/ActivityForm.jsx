e671678e8cac665f314578d0a219c6f1c5995da7
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-09 17:23:24 -0500

    adjust ActivityForm styles

--- components/ActivityForm.jsx
+++ components/ActivityForm.jsx
@@ -4,29 +4,29 @@ const ActivityForm = () => {
             <h3>Add an activity</h3>
             <form className="activity-form">
                 <label htmlFor="activity-title">Title</label>
-                <input className="activity-form-input"
+                <input className="activity-input-short-text"
                 type="text"
                 id="activity-title"
                 name="activity-title"
                 ></input>
-                <label htmlFor="activity-price">Price</label>
-                <input className="activity-form-input"
-                type="number"
-                id="activity-price"
-                name="activity-price"
-                placeholder="0"
-                ></input>
                 <label htmlFor="activity-address">Address</label>
-                <input className="activity-form-input"
+                <input className="activity-input-short-text"
                 type="text"
                 id="activity-address"
                 name="activity-address"
                 ></input>
-                <label htmlFor="activity-info">info</label>
-                <input className="activity-form-input"
+                <label htmlFor="activity-info">Info</label>
+                <input className="activity-input-long-text"
                 type="text"
                 id="activity-info"
                 name="activity-info"
+                ></input>
+                <label htmlFor="activity-price">Price</label>
+                <input className="activity-form-price"
+                type="number"
+                id="activity-price"
+                name="activity-price"
+                placeholder="0"
                 ></input>
                 <button className="btn-input-base-styling">Create</button>
             </form>
