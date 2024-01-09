77bf370348c6c4776a6f53b00b551d8659b2ed05
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-09 17:28:52 -0500

    rename ActivityForm input classNames

--- components/ActivityForm.jsx
+++ components/ActivityForm.jsx
@@ -4,25 +4,25 @@ const ActivityForm = () => {
             <h3>Add an activity</h3>
             <form className="activity-form">
                 <label htmlFor="activity-title">Title</label>
-                <input className="activity-input-short-text"
+                <input className="small-text-input"
                 type="text"
                 id="activity-title"
                 name="activity-title"
                 ></input>
                 <label htmlFor="activity-address">Address</label>
-                <input className="activity-input-short-text"
+                <input className="small-text-input"
                 type="text"
                 id="activity-address"
                 name="activity-address"
                 ></input>
                 <label htmlFor="activity-info">Info</label>
-                <input className="activity-input-long-text"
+                <input className="large-text-input"
                 type="text"
                 id="activity-info"
                 name="activity-info"
                 ></input>
                 <label htmlFor="activity-price">Price</label>
-                <input className="activity-form-price"
+                <input className="price-input"
                 type="number"
                 id="activity-price"
                 name="activity-price"
