ff835c95d1f887ecff05273e4edaa2ff3305919b
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-15 14:26:51 -0500

    correct spelling error

--- components/activityCard.jsx
+++ components/activityCard.jsx
@@ -1,7 +1,7 @@
 const ActivityCard = () => {
     return (
         <section className="activity-card">
-            <h3>Activiry Card</h3>
+            <h3>Activity Card</h3>
         </section>
     )
 };
